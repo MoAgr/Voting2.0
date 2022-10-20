c7f114263e67e7a132de4a6ad37747cd01c70273
Remix Workspace <>
2022-10-20 22:12:18 +0545

    Creating Poll Functionality Added

--- Voting.sol
+++ Voting.sol
@@ -3,31 +3,54 @@ pragma solidity ^0.8.0;
 
 contract Voting{
 
-    struct voter{
+    uint8 globalPollNo=0; 
+
+    struct Voter{
         string name;
         address addr;
         bool voted;
     }
 
-    struct option{
+    struct Option{
         string name;
         uint8 votes;
-        voter[] voters;
+        Voter[] voters;
     }
 
-    struct poll{
+    struct Poll{
         address creator;
         uint8 pollNo;
         uint8 nOptn;
-        option[] options;
+        Option[] options;
         uint startTime;
         uint endTime;
     }
 
-    poll[] public polls;
+    Poll[] public polls;
 
+    Option temp;
+    Poll tempPoll;
 
-    function createPoll(option[] memory options, uint start, uint end)public{
+    function createPoll(string[] memory options, uint start, uint end)public{ //check memory or storage
+        globalPollNo++;
+
+        Poll storage _tempPoll=tempPoll;
+        tempPoll.creator=msg.sender;
+        tempPoll.pollNo=globalPollNo;
+        tempPoll.startTime=start;
+        tempPoll.endTime=end;
+
+        //check this code snippet for gas
+
+        Option storage _temp=temp;
+        for(uint8 i=0;i<options.length;i++){
+            _temp.name=options[i];
+            _tempPoll.options.push(_temp);
+        }
+
+        //end of snippet
+
+        polls.push(_tempPoll);
 
     }
 
